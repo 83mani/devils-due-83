e6b1f9925c2ecf2e39144258661caab2287cba79
83mani <87688836+83mani@users.noreply.github.com>
2021-07-20 12:41:51 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-manis1.c54nh4jkdu0t.us-east-2.rds.amazonaws.com"
+customuser = "adm_mani83"
+custompass = "shalimar145"
+customdb = "maninder_db1"
+custombucket = "addmyspace"
+customregion = "us-east-2"
